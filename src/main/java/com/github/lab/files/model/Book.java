2f90f4c4979165a5ba27cad8ea5e3ba7c445e4e2
Libs <libosong@wt5656.com>
2024-08-29 10:58:57 +0800

    Initial commit

---
+++ src/main/java/com/github/lab/files/model/Book.java
@@ -0,0 +1,5 @@
+package com.github.lab.files.model;
+
+public record Book(int id, String title, String author, String status)
+{
+}
---
+++ src/main/java/com/github/lab/files/repository/BookRepository.java
@@ -0,0 +1,48 @@
+package com.github.lab.files.repository;
+
+import com.github.lab.files.model.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class BookRepository
+{
+	private Map<Integer, Book> map = new ConcurrentHashMap<>();
+
+	public BookRepository()
+	{
+		map.put(1, new Book(1, "Gulliver's Travels", "Jonathan Swift", "READING"));
+		map.put(2, new Book(2, "Oliver Twist", "Charles Dickens", "READING"));
+	}
+
+	public Map<Integer, Book> getAllBooks()
+	{
+		return map;
+	}
+
+	public Book findById(int id)
+	{
+		return map.get(id);
+	}
+
+	public Book save(Book book)
+	{
+		Book copy = new Book(book.id(), book.title(), book.author(), book.status());
+		map.put(book.id(), copy);
+		return copy;
+	}
+
+	public void delete(Integer id)
+	{
+		map.remove(id);
+	}
+
+	public Book update(Book book)
+	{
+		Book copy = new Book(book.id(), book.title(), book.author(), book.status());
+		map.put(book.id(), copy);
+		return copy;
+	}
+}
